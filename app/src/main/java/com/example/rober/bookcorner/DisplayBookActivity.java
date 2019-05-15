564621ef90b6ad70aa94bcc9e9ea5ccf7f1ceb6b
RobertTudorache <roberttudorache81@gmail.com>
2019-05-15 22:14:48 +0300

    Preluare date din firebase + afisarea lor in listview; Creare activitate pt afisarea unei carti + functionalitate pe butoanele de setare a cantitatii comandate;

---
+++ app/src/main/java/com/example/rober/bookcorner/DisplayBookActivity.java
@@ -0,0 +1,115 @@
+package com.example.rober.bookcorner;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rober.bookcorner.classes.Carte;
+
+public class DisplayBookActivity extends AppCompatActivity {
+
+    TextView textView_display_book_autor;
+    TextView textView_display_book_titlu;
+    TextView textView_display_book_aprecieri;
+    TextView textView_display_book_descriere;
+    TextView textView_display_book_cantitate_adaugata;
+    TextView textView_display_book_cantitate;
+
+    Button button_display_book_minus;
+    Button button_display_book_plus;
+    Button button_display_book_add;
+    private Carte carte;
+    private int cantitateAdaugata = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_book);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        this.textView_display_book_autor = findViewById(R.id.textView_display_book_autor);
+        this.textView_display_book_titlu = findViewById(R.id.textView_display_book_titlu);
+        this.textView_display_book_aprecieri = findViewById(R.id.textView_display_book_aprecieri);
+        this.textView_display_book_descriere = findViewById(R.id.textView_display_book_descriere);
+        this.textView_display_book_cantitate_adaugata = findViewById(R.id.textView_display_book_cantitate_adaugata);
+        this.textView_display_book_cantitate_adaugata.setText(String.valueOf(cantitateAdaugata));
+        this.textView_display_book_cantitate = findViewById(R.id.textView_display_book_cantitate);
+
+        populareInterfata();
+
+        this.button_display_book_minus = findViewById(R.id.button_display_book_minus);
+        this.button_display_book_plus = findViewById(R.id.button_display_book_plus);
+        this.button_display_book_add = findViewById(R.id.button_display_book_add);
+
+        FloatingActionButton fab = findViewById(R.id.fab_activity_display_book);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        button_display_book_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scadeCantitate();
+            }
+        });
+
+        button_display_book_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adaugaCantitate();
+            }
+        });
+
+        button_display_book_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo: Adaugare in map-ul din activitatea cosului de cumparaturi
+            }
+        });
+    }
+
+    private void populareInterfata() {
+        this.carte = getCarteFromIntent();
+        this.textView_display_book_autor.setText(carte.getAutor());
+        this.textView_display_book_titlu.setText(carte.getTitlu());
+        this.textView_display_book_aprecieri.setText(String.valueOf(carte.getNrAprecieri()));
+        this.textView_display_book_descriere.setText(carte.getDescriere());
+        this.textView_display_book_cantitate.setText(String.valueOf(carte.getCantitate()));
+    }
+
+    private Carte getCarteFromIntent() {
+        Carte carte = (Carte) getIntent().getSerializableExtra("Carte");
+        return carte;
+    }
+
+    private void scadeCantitate() {
+        if (!(this.cantitateAdaugata == 1)) {
+            this.cantitateAdaugata--;
+            this.textView_display_book_cantitate_adaugata.setText(String.valueOf(this.cantitateAdaugata));
+        }
+    }
+
+    private void adaugaCantitate() {
+        if (!(this.cantitateAdaugata == this.carte.getCantitate())) {
+            this.cantitateAdaugata++;
+            this.textView_display_book_cantitate_adaugata.setText(String.valueOf(this.cantitateAdaugata));
+        } else {
+            Toast.makeText(getBaseContext(), "Cantiatea maxima a fost atinsa", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
--- app/src/main/java/com/example/rober/bookcorner/MainActivity.java
+++ app/src/main/java/com/example/rober/bookcorner/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rober.bookcorner;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,9 +15,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.rober.bookcorner.classes.BookList;
+import com.example.rober.bookcorner.classes.Carte;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
+    ListView listViewBooks;
+    List<Carte> bookList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +45,10 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        myRef = database.getReference("afaceri");
+        listViewBooks = findViewById(R.id.listViewBooks);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +67,47 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String categorie = getIntent().getStringExtra("NUME_CATEGORIE");
+
+        if (Objects.nonNull(categorie)) {
+            myRef = database.getReference(categorie);
+
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    bookList.clear();
+                    for(DataSnapshot d : dataSnapshot.getChildren()) {
+                        Carte carte = d.getValue(Carte.class);
+                        bookList.add(carte);
+                    }
+
+                    BookList adapter = new BookList(MainActivity.this,bookList);
+                    listViewBooks.setAdapter(adapter);
+
+                    listViewBooks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            // TODO: 5/9/2019 Creare intent catre activitatea de afisare a unei carti + pasarea cartii catre activitate
+                            Intent intent = new Intent(MainActivity.this, DisplayBookActivity.class);
+                            intent.putExtra("Carte", bookList.get(position));
+                            startActivity(intent);
+                            //Toast.makeText(MainActivity.this, bookList.get(position).getTitlu(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 
     @Override
@@ -81,19 +149,25 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         // TODO: 5/2/2019 Adaugare noi activitati 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+        if (id == R.id.nav_categorie_sanatate) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("NUME_CATEGORIE", "sanatate");
+            startActivity(intent);
+        } else if (id == R.id.nav_categorie_dezvoltare_personala) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("NUME_CATEGORIE", "dezvoltarePersonala");
+            startActivity(intent);
+        } else if (id == R.id.nav_categorie_actiune) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("NUME_CATEGORIE", "fictiune");
+            startActivity(intent);
+        } else if (id == R.id.nav_categorie_afaceri) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("NUME_CATEGORIE", "afaceri");
+            startActivity(intent);
+        } else if (id == R.id.nav_categorie_preferate) {
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
